5bdc96c5123f7d322282b69346020c9df55c0ab1
Ebony Sharrock <ebonysharrock@gmail.com>
2019-12-12 08:26:25 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,34 @@
 class Owner
-  # code goes here
+   pets=[]
+   @@all=[]
+   attr_reader :name, :species
+   def initialize(name)
+     @name=name
+     @species="human"
+     @@all<<self
+   end
+   def say_species
+     human = self.species
+     p "I am a #{human}."
+   end
+   def self.all
+     @@all
+   end
+   def self.count
+     @@all.size
+   end
+   def self.reset_all
+     @@all.clear
+   end
+   def cats
+     Cat.all.collect {|cat| cat.owner.name == self.name}
+   end
+  def buy_a_pet
+  end
+  def walk #changes mood
+  end
+   def feed #changes mood
+   end
+   def sell_pets
+   end
 end
